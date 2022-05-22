44fe4d24867d03eebec8c99c84b6dff38c6dd81f
AAgottani <adilson.reis@pucpr.edu.br>
2022-05-22 17:57:59 -0300

    primeiras páginas

---
+++ app/src/main/java/com/example/laborconecta/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.laborconecta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView user, password;
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user= findViewById(R.id.userEditText);
+        password= findViewById(R.id.passwordEditText);
+        loginBtn= findViewById(R.id.loginBtn);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent= new Intent(MainActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+    }
+
+
+
+}
