c4e47c5f0daa987f665dedf548b56773e662f292
Gustavo Lidani <lidanig0@gmail.com>
2022-05-23 17:06:16 -0300

    user profile and logout function

--- app/src/main/java/com/example/laborconecta/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/laborconecta/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.laborconecta.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.laborconecta.LoginActivity;
+import com.example.laborconecta.MainActivity;
 import com.example.laborconecta.R;
+import com.example.laborconecta.RegisterActivity;
+import com.example.laborconecta.dbhelpers.SessionHelper;
+import com.example.laborconecta.repositories.UserRepository;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +25,54 @@ import com.example.laborconecta.R;
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final UserRepository userRepository;
 
     public ProfileFragment() {
-        // Required empty public constructor
+        userRepository = UserRepository.getInstance(getContext());
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView name = view.findViewById(R.id.profile_name);
+        TextView email = view.findViewById(R.id.profile_email);
+
+        if (userRepository.getUser() != null) {
+            name.setText(userRepository.getUser().name);
+            email.setText(userRepository.getUser().email);
+        }
+
+        Button logout = view.findViewById(R.id.logout);
+        logout.setOnClickListener(v -> {
+            if (getActivity() == null || userRepository.getUser() == null) {
+                return;
+            }
+
+            SessionHelper session = new SessionHelper(getContext());
+            session.invalidate(userRepository.getUser().id);
+            userRepository.setUser(null);
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            getActivity().finish();
+        });
+
+        return view;
     }
 }
