6a56683a86326dc2f5941049cfc58fefad483876
Gustavo Lidani <lidanig0@gmail.com>
2022-05-23 17:07:38 -0300

    User singleton instance to keep user loged available all over the app

--- app/src/main/java/com/example/laborconecta/repositories/UserRepository.java
+++ app/src/main/java/com/example/laborconecta/repositories/UserRepository.java
@@ -1,2 +1,38 @@
-package com.example.laborconecta.repositories;public class UserRepository {
+package com.example.laborconecta.repositories;
+
+import android.content.Context;
+
+import com.example.laborconecta.contracts.UserContract;
+
+import org.jetbrains.annotations.Nullable;
+
+public final class UserRepository {
+
+    private static UserRepository instance;
+
+    private @Nullable UserContract.UserModel user;
+
+    private UserRepository(Context context) {
+    }
+
+    public boolean isLogged() {
+        return this.user != null;
+    }
+
+    public UserContract.UserModel getUser() {
+        return user;
+    }
+
+    public void setUser(@Nullable UserContract.UserModel user) {
+        this.user = user;
+    }
+
+    public static UserRepository getInstance(Context context) {
+
+        if (instance == null) {
+            instance = new UserRepository(context);
+        }
+
+        return instance;
+    }
 }
