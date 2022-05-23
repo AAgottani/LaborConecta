db900473851e794b59a87cadd02efcd33523477a
Gustavo Lidani <lidanig0@gmail.com>
2022-05-23 17:05:56 -0300

    empty fragments

--- app/src/main/java/com/example/laborconecta/fragments/MyServicesFragment.java
+++ app/src/main/java/com/example/laborconecta/fragments/MyServicesFragment.java
@@ -34,16 +34,14 @@ public class MyServicesFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MyServicesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MyServicesFragment newInstance(String param1, String param2) {
+    public static MyServicesFragment newInstance() {
         MyServicesFragment fragment = new MyServicesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
